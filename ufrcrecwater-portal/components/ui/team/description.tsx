c480f22023bd3d7d4cad1004fc24e00f43521909
akhilreddy6g <akhilreddyoff21@gmail.com>
2026-01-10 18:57:36 -0500

    Revert "Add team page architecture and mock data"
    This reverts commit 55ccb204c36394b7afa72aad475f3ea48a3f794b.

--- ufrcrecwater-portal/components/ui/team/description.tsx
+++
@@ -1,7 +0,0 @@
-import { MemDesc } from "@/types/team";
-
-export default function MemberDescription(props: MemDesc){
-    return (
-        <p key={props.id + "_desc"} className="text-sm font-medium text-[#aeabab] px-2">{props.description}</p>
-    )
-}   
